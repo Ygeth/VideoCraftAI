4f8acb5f47574c7d972111d30449decd60833d28
Ricardo Montaner <lewislopezmontaner@gmail.com>
2025-09-10 07:26:08 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+
+const firebaseConfig = {
+  "projectId": "studio-7532894983-5fe8e",
+  "appId": "1:467844479955:web:76c0be94bf310347004d26",
+  "storageBucket": "studio-7532894983-5fe8e.firebasestorage.app",
+  "apiKey": "AIzaSyBmvfOtG0QhfRGxBiMNOmUioqR8C43_qn8",
+  "authDomain": "studio-7532894983-5fe8e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "467844479955"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
