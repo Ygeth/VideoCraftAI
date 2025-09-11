438f86c69f76b13b092c22aa3da6e8ba12f717f7
Ricardo Montaner <lewislopezmontaner@gmail.com>
2025-09-11 15:04:23 +0000

    Ahora comenta los usos de kokoro y utiliza kokoro_huggings

--- src/services/kokoro/index.ts
+++ src/services/kokoro/index.ts
@@ -1,6 +1,7 @@
 // src/services/kokoro/index.ts
 
 import { Kokoro } from './Kokoro';
+import { KokoroHugging } from './kokoro_hugging';
 import { kokoroModelPrecision } from "./kokoroConfig";
 
 let precision: kokoroModelPrecision = "fp32";
@@ -8,4 +9,5 @@ let precision: kokoroModelPrecision = "fp32";
 // Inicializamos Kokoro aquí. El `await` en el nivel superior
 // asegura que cualquier módulo que importe `kokoroInstance`
 // esperará a que la inicialización se complete.
-export const kokoroInstance = await Kokoro.init(precision);
+// export const kokoroInstance = await Kokoro.init(precision);
+export const kokoroInstance = await KokoroHugging.init(precision);
